8b243fc29a9653eeeb19269aa8a4ebf18a3066e0
moises eastland <dewaynepade95@gmail.com>
2019-05-24 19:27:03 -0500

    corrected itnitial oversight in survey.html file/writing catch all route to home.html

--- htmlRoutes.js
+++ htmlRoutes.js
@@ -1,11 +1,11 @@
 const path = require('path');
 
 module.exports =function(app) {
-    app.get("/", function(req,res){
+    app.get("/survey", function(req,res){
         res.sendFile(path.join(_dirname, "../public/survey.html"));
     });
 
-    app.get("/", function(req,res){
-        res.sendFile(path.join(_dirname,"../public/home.html"));
+    app.get("*",function(req,res){
+        res.senFile(path.join(_dirname,"../public/home.html"));
     });
 }
